15e01a025cb05842297b2f7688d57317cac0152e
ManaliMhjn <107684751+ManaliMhjn@users.noreply.github.com>
2023-05-18 15:36:25 +0000

    Added Dynamic Data and API

--- app.py
+++ app.py
@@ -1,12 +1,46 @@
 print("Helllo World")
 # flask is web framework of python
 # from module flask import Flask -->jo ki ek class hai
-from flask import Flask
-# creating an object of the class humne sari functinality of class Flask import kra krr app me dal di hai 
+from flask import Flask, render_template
+# creating an object of the class humne sari functinality of class Flask import kra krr app me dal di hai
 # jab bhi url access ho  show hello world
-app=Flask(__name__)
+app = Flask(__name__)
+JOBS = [
+  {
+    'id': 1,
+    'Role': 'Data Analyst',
+    'Location': 'Bengluru,India',
+    'Salary': '10,00,000'
+  },
+  {
+    'id': 2,
+    'Role': 'Data Scientist',
+    'Location': 'Delhi,India',
+    'Salary': '12,00,000'
+  },
+  {
+    'id': 3,
+    'Role': 'Front-End Developer',
+    'Location': 'Bengluru,India',
+    'Salary': '15,00,000'
+  },
+  {
+    'id': 1,
+    'Role': 'Back-End Developer',
+    'Location': 'Pune,India',
+    'Salary': '17,00,000'
+  },
+]
+
+
 @app.route("/")
 def hello_world():
-  return "hello World_____Helloo agian"
-if __name__=='__main__':
-  app.run(host='0.0.0.0',debug=True)
+  return render_template('home.html', jobs=JOBS)
+
+ 
+
+
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True)
